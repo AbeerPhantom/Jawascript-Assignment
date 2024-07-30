761cd84c62052e2ebba2dec5a340a35e0188363c
Syed Abeer Ali <syedabeerali03@gmail.com>
2024-07-30 14:52:33 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,681 @@
+
+                                                             // CHAPTER NO 1 ALERT
+
+//                                                   QUESTION NO 1
+
+// alert("First Name");
+// alert("Last Name");
+// alert("Email");
+// alert("Phone Number");
+// alert("Password");
+
+//                                                     QUESTION NO 2
+
+
+
+// alert("You're learning JavaScript!");
+
+
+//                                                     QUESTION NO 3
+
+
+
+// alert("Hello! My name is Syed Abeer Ali. I hope you doing well.");
+
+
+
+//                                                Chapter 2 (Variables for string) 
+
+//                                                      QUESTION NO 1
+
+// var userName;
+
+//                                                        QUESTION NO 2
+
+// var carName;
+// carName = 'Civic';
+
+
+//                                                       QUESTION NO 3
+
+// var teamName = "SMART XI";
+// alert(teamName);
+
+//                                                        QUESTION NO 4
+
+// var bestMan = "Charlie";
+// bestMan = "Viart Kohli";
+
+
+
+//                                                     Chapter 3 (Variables for numbers) 
+
+//                                                       QUESTION NO 1
+
+// var caseQty;
+
+//                                                      QUESTION NO 2
+
+// caseQty = 144;
+
+//                                                         QUESTION NO 3
+
+// var num = 9;
+
+
+//                                                           QUESTION NO 4
+
+// var total;
+// total = 5 + 10;
+
+//                                                             QUESTION NO 5
+
+// var merchTotal = 100; 
+// var shippingCharge = 10; 
+// var orderTotal = merchTotal + shippingCharge; 
+
+// Answer is 110
+
+//                                                                QUESTION NO 6
+
+// var score = 20;
+// score = score + 15;
+
+//                                                            Chapter 4 (Variable names Legal and  Illegal) 
+
+//                                                               QUESTION NO 1
+
+// var productCost = 3.45;
+
+//                                                            QUESTION NO 2
+
+// var nameOfBand;
+
+//                                                              QUESTION NO 3
+
+// var totalBikes = 45;
+
+//                                                              QUESTION NO 4
+
+// var abeerAli; 
+
+
+//                                                               QUESTION NO 5
+
+// Legal Variables
+
+// firstName
+// lastName
+// userAge
+// _score
+// $amount
+// totalCost
+
+// Illegal Variables
+
+// 1stPlace 
+// user-name 
+// var 
+// first name 
+// 123variable
+
+//                                                      Chapter 5 (Math Expression I) 
+
+
+                                                             // QUESTION NO 1
+
+// MODULUS ( % )
+
+                                                                // QUESTION NO 2 
+
+                                                              // var num = 20 % 6;
+ 
+// alert(num);  Answer is 2
+
+                                                                    // QUESTION NO 3
+
+// var largeNum = 1000 * 2000;
+
+                                                                     // QUESTION NO 4
+
+// var a = 10;
+// var b = 5;
+// var sub = a - b;
+
+                                                                         // QUESTION NO 5
+
+// var remainder = 17 % 4;
+
+                                                                      // QUESTION NO 6
+
+// alert(2 * 5);
+
+
+//                                                          Chapter 6 (Math Expression II) 
+
+
+                                                                   // QUESTION NO 1
+ 
+// x += 1;
+
+// x++;
+
+// QUESTION NO 2
+
+// var x = 100;
+// x--;
+
+// QUESTION NO 3
+
+// var y = x++; 
+// What is the value of y? 
+
+// Answer is 51
+
+// QUESTION NO 4
+
+// var z = --y; 
+// What is the value of z? 
+
+// Answer is 49
+
+
+// QUESTION NO 5
+
+// var num = 10;
+// var newNum = num--;
+
+
+// QUESTION NO 6
+
+// var originalValue = 5;
+// var newValue = originalValue++;
+
+
+// QUESTION NO 7
+
+// let num = 7;
+// num++;
+// alert(num);
+
+
+//                                                                Chapter 7 (Math Expression III) 
+
+
+// QUESTION NO 1
+
+
+// The value of calculatedNum is 14.
+
+
+// QUESTION NO 2
+
+// The value of calculatedNum is 24.
+
+
+// QUESTION NO 3 
+
+
+// The value of calculatedNum is 24.
+
+
+// QUESTION NO 4
+
+
+// The value of calculatedNum is 32.
+
+
+// QUESTION NO 5
+
+// var cost = (2 + 2) * (4 + 10);
+
+
+// QUESTION NO 6
+
+// var units = 2 + (2 * 4) + 10;
+
+
+// QUESTION NO 7
+
+// cannot be possible
+
+
+//                                               Chapter 8 (Concatenating Text  Strings) 
+
+
+// QUESTION NO 1
+
+// Answer is 22
+
+// QUESTION NO 2
+
+// var message = ("Hello," + "Dolly"); 
+// alert (message);
+
+// Answer is Hello,Dolly
+
+
+// QUESTION NO 3
+
+// Answer is 333
+
+
+// QUESTION NO 4
+
+// alert("Pakistan" + " " + "Zindabad");
+
+
+// QUESTION NO 5
+
+// var amount = "The total is " + 42;
+
+
+// QUESTION NO 6
+
+// var firstName = "John"; 
+// var lastName = "Doe";
+// var fullName = firstName + " " + lastName;
+
+
+//                                                            Chapter 9 (Prompts) 
+
+// QUESTION NO 1
+
+// var firstName = prompt("Enter first name");
+// alert (firstName)
+
+// QUESTION NO 2
+
+// var country = prompt("Country?", "China");
+
+
+// QUESTION NO 3
+
+// var yourName = prompt("Enter Your Name"); 
+
+
+// QUESTION NO 4
+
+// var color = prompt("What is your favorite color?", "Blue");
+
+
+// QUESTION NO 5
+
+// var message = "What's your favorite food?";
+// var defaultResponse = "Pizza";
+// var userResponse = prompt(message, defaultResponse);
+
+
+// QUESTION NO 6
+
+// var userResponse = prompt("What is your favorite hobby?", "Reading");
+// alert(userResponse);
+
+
+//                                                         Chapter 10 (if statments) 
+
+// QUESTION NO 1
+
+// var city = "Karachi" 
+// if (city == "Karachi") { 
+// alert("The City OF Lights"); 
+// } 
+
+
+// QUESTION NO 2
+
+// var x= 8;
+// var y= 8;
+// if (x === y) {
+//     var z = prompt("What is the value of z?");
+// }
+
+
+// QUESTION NO 3
+
+// if (ZipCode === "10010") {
+//     alert("Karachi");
+// } else {
+//     alert("Please write correct city");
+// }
+
+
+// QUESTION NO 4
+
+// var x = 1;
+
+// if (x === 1) {
+//     x = 2;
+// }
+
+//                                                    Chapter 11 (Comparison Operators) 
+
+
+// QUESTION NO 1
+
+// if (D !== C) {}
+
+// QUESTION NO 2
+
+// if (E >= F) {
+
+// }
+
+
+
+// QUESTION NO 3
+
+// var num = 5;
+
+// if (num !== 10) {
+//     num = 10;
+// }
+
+
+// QUESTION NO 4
+
+// var number1 = 3;
+// // var number2 = 5;
+
+// // if (number1 !== number2) {
+// //     alert("Congratulations!");
+// }
+
+// QUESTION NO 5
+
+// let Name = prompt("Enter your name");
+
+// if (Name !== "Ali") {
+//     alert("No match");
+// }
+
+
+
+//                                          Chapter 12 (if…else and else if  statements) 
+
+// QUESTION NO 1
+
+// var value1 = 10;
+// var value2 = 5;
+
+// if (value1 >= value2) {
+//     alert("Value1 is greater than or equal to Value2");
+// } else {
+//     alert("Value1 is less than Value2");
+// }
+
+
+// QUESTION NO 2
+
+// var English ;
+// English = +prompt ("Enter English marks:");
+// var Islamiat ;
+// Islamiat = +prompt ("Enter Islamiat marks:");
+// var Maths;
+// Maths = +prompt ("Enter Maths marks:");
+// var Urdu ;
+// Urdu = +prompt ("Enter  Urdu marks:");
+// var TotalMarks;
+// TotalMarks= English + Urdu + Islamiat + Maths;
+// var perc;
+// percentage = (TotalMarks/400)*100;
+// var grade = '' ;
+
+// if (percentage >=80 ) {
+//     grade = 'A+';
+// } else if (percentage >= 70 && percentage >= 79) {
+//     grade = 'A';
+// } else if (percentage >= 60 && percentage >= 69) {
+//     grade = 'B';
+// } else if (percentage >= 50 && percentage >= 59) {
+//     grade = 'C';
+// } else if (percentage >= 40 && percentage >= 49) {
+//     grade = 'D';
+// } 
+// else {
+//     grade = 'F';
+// }
+// var result = 'Total Marks: ' + TotalMarks + '\n' + 
+// 'Percentage: ' + percentage.toFixed(2) + '\n' +
+// 'Grade: ' + grade;
+
+// alert(result);
+
+
+// QUESTION NO 3
+
+// var a = 7;
+
+// if (a === 10) {
+//     alert("a is 10");
+// } else {
+//     alert("The correct value of a is " + a);
+// }
+
+
+
+// QUESTION NO 4
+
+// var city = prompt("Enter a city");
+// if (city === "Karachi") {
+//     alert("It is Karachi");
+// } else if (city === "Lahore") {
+//     alert("It is Lahore");
+// } else {
+//     alert("City not recognized");
+// }
+
+     
+//                                                      Chapter 13 (Testing sets of conditions) 
+   
+
+
+// QUESTION NO 1
+
+// if (a === b && c === d) {}
+
+
+// QUESTION NO 2
+
+// if (a === b || c !== d) {
+
+
+// QUESTION NO 3
+    
+// if (Name === "Hamza" || Name === "Arsalan") {}
+// if (age < 60) {
+
+
+// QUESTION NO 4
+
+// var num1 = 5;
+// var num2 = 10;
+
+// if (num1 < num2 || num1 > num2) {
+//     alert("The condition is true");
+// }
+
+
+
+// QUESTION NO 5
+
+// var firstName = "Syed";
+// var lastName = "Abeer";
+
+// let userFirstName = prompt("Enter your first name");
+// let userLastName = prompt("Enter your last name");
+
+// if (userFirstName === firstName && userLastName === lastName) {
+//     alert("Welcome, " + firstName + " " + lastName);
+// }
+
+
+//                                                         Chapter 14 (If statements nested) 
+
+
+
+// QUESTION NO 1
+
+// var password = "secret";
+
+// if (password !== "") {
+//     if (password.length <= 5) {
+//         alert("Password must be greater than 5");
+//     } else {
+//         alert("OK");
+//     }
+// }
+
+
+// QUESTION NO 2
+
+// var a = 1;
+// var c = 'Max'
+// if (a === 1) {
+//     if (c === "Max") {
+//         alert("OK");
+//     }
+// }
+
+
+
+// QUESTION NO 2
+
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+// }
+
+
+
+
+// QUESTION NO 4
+
+// let num1 = 10;
+// let num2 = 10;
+
+// if (num1 === num2) {
+//     if (num1 <= num2) {
+//         alert("Both conditions are true");
+//     }
+// }
+
+
+//                                                                Chapter 15 (Array I) 
+
+
+
+//  QUESTION NO 1
+
+// var empty = [];
+
+
+// QUESTION NO 2
+
+// var final = ['Hello'];
+
+
+// QUESTION NO 3
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]);
+
+
+// QUESTION NO 4
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// alert(alphabet.length);
+
+
+
+// QUESTION NO 5
+
+// var myArray = ["First Element"];
+// myArray[1] = "Second Element";
+// alert(myArray[1]);
+
+
+//                                                              Chapter 16 (Array II) 
+
+
+
+// QUESTION NO 1
+
+// var myArray = ["First Element"];
+// myArray.push("Second Element");
+// alert(myArray[myArray.length - 1]);
+
+
+// QUESTION NO 2
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.pop();
+
+
+// QUESTION NO 3
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(5);
+
+
+
+                                                                  // Chapter 16 (Array III) 
+
+
+
+
+// QUESTION NO1
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+
+
+// QUESTION NO 2
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(1, 2, 3);
+// alert(sizes);
+
+
+// QUESTION NO 3
+
+// var myArray = ["Farhan"];
+// myArray.unshift("Shahzad");
+// alert(myArray[0]);
+
+
+// QUESTION NO 4
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2, 0, "L");
+// alert(sizes);
+
+
+
+// QUESTION NO 5
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0, 3);
+// alert(regSizes);
+
+
+
+// QUESTION NO 6
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 3, "parrot", "cow");
+// alert(pets);
+
+
+
+// QUESTION NO 7
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 2);
+// alert(pets);
+
+
+// QUESTION NO 8
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// var reducedPets = pets.slice(3, 5);
+// alert(reducedPets );
